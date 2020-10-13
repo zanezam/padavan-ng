b2c484b7173a75d0da3be162b59631b263ee65b1
IG-AI <d.e.agstrand@gmail.com>
2020-10-13 14:11:06 +0200

    Fixed error in the generating of man for curl by adding missing man doc for alt-svc

---
+++ trunk/libs/libcurl/curl-7.6X.X/docs/cmdline-opts/alt-svc.d
@@ -0,0 +1,9 @@
+Long: alt-svc
+Arg: <protocol-id><max-age><persist=1>
+Protocols: HTTP
+Help: Allows an origin's resources to be authoritatively available at a separate network location.
+---
+Alternative Services for HTTP, which allow an origin's resources 
+to be authoritatively available at a separate network 
+location, possibly accessed with a different protocol configuration.
+
